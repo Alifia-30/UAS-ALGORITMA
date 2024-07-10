e4de7b1dfce8045822d57b8a6d225823b903325a
Alifia Shasa <145659930+Alifia-30@users.noreply.github.com>
2024-07-10 12:05:57 +0700

    Update nomor_empat.py

--- nomor_empat.py
+++ nomor_empat.py
@@ -23,13 +23,3 @@ class Mangga(Buah):
 
     def info(self):
         return f"Nama: {self.nama}, Warna: {self.warna}, Rasa: {self.rasa}, Vitamin: {self.vitamin}"
-
-mangga1 = Mangga("Mangga Arumanis", "Hijau", "Manis", "Vitamin C")
-
-print(mangga1.info())
-
-mangga1.set_nama("Mangga Harum Manis")
-mangga1.set_warna("Kuning")
-mangga1.set_rasa("Manis Asam")
-
-print(mangga1.info())
